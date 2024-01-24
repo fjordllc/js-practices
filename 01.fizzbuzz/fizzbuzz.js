a9043c5646c2fc0132ab212ecf7da6983548ca49
BEING\inatomi <inatomi@beingcorp.co.jp>
2024-01-24 17:21:56 +0900

    fizzbuzz1回目

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,19 @@
+/*
+let point='num';
+let point3='fizz';
+let point5='buzz';
+let point15='fizzbuzz';
+*/
+
+for(let num = 1;num<=20;num++){
+
+    if (num % 3 === 0 && num % 5 !== 0) {
+      console.log("fizz");
+    } else if (num % 3 !== 0 && num % 5 === 0) {
+      console.log ("buzz");
+    } else if (num % 3 === 0 && num % 5 === 0) {
+      console.log ("fizzbuzz");
+    } else {
+      console.log (num);
+    }
+  }
