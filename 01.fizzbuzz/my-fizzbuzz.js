b8cabeca72f145a3e108a0975cf168a992e39390
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-05-10 11:07:04 +0900

    ループカウンタの始まりを１に変更

--- 01.fizzbuzz/my-fizzbuzz.js
+++ 01.fizzbuzz/my-fizzbuzz.js
@@ -1,4 +1,4 @@
-for (let i = 0; i <= 20; i++) {
+for (let i = 1; i <= 20; i++) {
   if (i % 15 === 0) console.log("FizzBuzz");
   else if (i % 3 === 0) console.log("Fizz");
   else if (i % 5 === 0) console.log("Buzz");
