4d0ab9632642960064de9fb4b88dc715ee0539fc
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-05-23 16:11:13 +0900

    スネークケースをキャメルケースに変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -3,21 +3,21 @@ const today = new Date();
 const { m, y } = minimist(process.argv.slice(2));
 const month = m ? m - 1 : today.getMonth();
 const year = y ? y : today.getFullYear();
-const display_month = month + 1;
-const first_day = new Date(year, month, 1).getDay();
-const last_date = new Date(year, month + 1, 0).getDate();
+const displayMonth = month + 1;
+const firstDay = new Date(year, month, 1).getDay();
+const lastDate = new Date(year, month + 1, 0).getDate();
 const weeks = new Array();
-let first_week = "";
-if (first_day !== 0) {
-  const padding = "  " + "   ".repeat(first_day - 1);
-  first_week = first_week.concat(padding);
+let firstWeek = "";
+if (firstDay !== 0) {
+  const padding = "  " + "   ".repeat(firstDay - 1);
+  firstWeek = firstWeek.concat(padding);
 }
-let week = first_week;
-for (let day = 1, weekday = first_day; day <= last_date; day++, weekday++) {
-  let number_width = 3;
-  if (weekday === 0) number_width = 2;
-  week = week.concat(String(day).padStart(number_width));
-  if (day === last_date || weekday === 6) {
+let week = firstWeek;
+for (let day = 1, weekday = firstDay; day <= lastDate; day++, weekday++) {
+  let numberWidth = 3;
+  if (weekday === 0) numberWidth = 2;
+  week = week.concat(String(day).padStart(numberWidth));
+  if (day === lastDate || weekday === 6) {
     weeks.push(week);
     weekday = -1;
     week = "";
@@ -25,10 +25,10 @@ for (let day = 1, weekday = first_day; day <= last_date; day++, weekday++) {
 }
 
 const calender = weeks.join("\n");
-const format_calender = [
-  `       ${display_month}月 ${year}`,
+const formatCalender = [
+  `       ${displayMonth}月 ${year}`,
   "日 月 火 水 木 金 土",
   `${calender}`,
 ].join("\n");
 
-console.log(format_calender);
+console.log(formatCalender);
