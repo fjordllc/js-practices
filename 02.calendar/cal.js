56dce7dfba5bc9e4777c31f526426551e0c9a1fd
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-05-23 13:03:38 +0900

    フォーマット用のformat_calender作成、ロジックを整理

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -3,39 +3,33 @@
 // const { m, y } = minimist(process.argv.slice(2));
 // const input_date = new Date(y, m);
 // console.log(input_date);
-
+// const today = new Date();
 const base_date = new Date();
 const base_year = base_date.getFullYear();
-const base_month = base_date.getMonth() + 1;
-const first_date = new Date(base_date.getFullYear(), base_date.getMonth(), 1);
-const last_date = new Date(
-  base_date.getFullYear(),
-  base_date.getMonth() + 1,
-  0,
-);
-const days = last_date.getDate();
-const first_day = first_date.getDay();
-let weekday = first_day;
-let line = "   ".repeat(first_day);
-let cal = new Array();
-for (let day = 1; day <= days; day++) {
-  line = line + String(day).padStart(3);
+const base_month = base_date.getMonth();
+const display_month = base_month + 1;
+const days = new Date(base_year, base_month + 1, 0).getDate();
+const first_day = new Date(base_year, base_month, 1).getDay();
+const weeks = new Array();
+let week = "  " + "   ".repeat(first_day - 1);
+for (let day = 1, weekday = first_day; day <= days; day++, weekday++) {
+  let number_width = 3;
+  if (weekday === 0) number_width = 2;
+  week = week.concat(String(day).padStart(number_width));
   if (day === days || weekday === 6) {
-    weekday = 0;
-    cal.push(line);
-    line = "";
-    continue;
+    weeks.push(week);
+    weekday = -1;
+    week = "";
   }
-  weekday += 1;
 }
-const text = cal.join("\n");
-console.log(`       ${base_month}月 ${base_year}`);
-console.log(" 日 月 火 水 木 金 土");
-console.log(text, "\n");
-console.log(base_date);
-console.log(base_date.toString());
-console.log(first_date.toString());
-console.log(last_date.toString());
-console.log(base_month);
+const calender = weeks.join("\n");
+const format_calender = [
+  `       ${display_month}月 ${base_year}`,
+  "日 月 火 水 木 金 土",
+  `${calender}`,
+].join("\n");
+
+console.log(format_calender);
+
 // 擬似Rangeを作る方法（今回は使用しないかも）
 // const get_days = (start, end) => [...Array(end + 1).keys()].slice(start);
