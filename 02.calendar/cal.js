40e9f37c096bbe78f7bac45383e85646edcf9a1a
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-05-24 09:00:23 +0900

    if文にブロック追加、変数名を変更、月の取得方法を変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,21 +1,26 @@
 import minimist from "minimist";
+
 const today = new Date();
 const { m, y } = minimist(process.argv.slice(2));
-const year = y || today.getFullYear();
-const displayMonth = m || today.getMonth();
-const month = m - 1;
-const firstDay = new Date(year, month, 1).getDay();
-const lastDate = new Date(year, month + 1, 0).getDate();
+const baseYear = y || today.getFullYear();
+const baseMonth = m || today.getMonth() + 1;
+const month = baseMonth - 1;
+const firstDay = new Date(baseYear, month, 1).getDay();
+const lastDate = new Date(baseYear, month + 1, 0).getDate();
 const weeks = new Array();
+
 let firstWeek = "";
 if (firstDay !== 0) {
   const padding = "  " + "   ".repeat(firstDay - 1);
   firstWeek = firstWeek.concat(padding);
 }
+
 let week = firstWeek;
 for (let day = 1, weekday = firstDay; day <= lastDate; day++, weekday++) {
   let numberWidth = 3;
-  if (weekday === 0) numberWidth = 2;
+  if (weekday === 0) {
+    numberWidth = 2;
+  }
   week = week.concat(String(day).padStart(numberWidth));
   if (day === lastDate || weekday === 6) {
     weeks.push(week);
@@ -26,7 +31,7 @@ for (let day = 1, weekday = firstDay; day <= lastDate; day++, weekday++) {
 
 const calender = weeks.join("\n");
 const formatCalender = [
-  `       ${displayMonth}月 ${year}`,
+  `       ${baseMonth}月 ${baseYear}`,
   "日 月 火 水 木 金 土",
   `${calender}`,
 ].join("\n");
