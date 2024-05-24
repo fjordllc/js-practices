fb3c8aa7c6c4c4d20edc1fccfc05d70041e54502
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-05-24 10:00:37 +0900

    ロジックをメソッド化

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,33 +1,39 @@
 import minimist from "minimist";
 
+const buildCalender = (baseYear, baseMonth) => {
+  const month = baseMonth - 1;
+  const firstDay = new Date(baseYear, month, 1).getDay();
+  const lastDate = new Date(baseYear, month + 1, 0).getDate();
+  const weeks = new Array();
+  let firstWeek = new String();
+  if (firstDay !== 0) {
+    firstWeek = firstWeek.concat("   ".repeat(firstDay));
+  }
+  let week = firstWeek;
+  for (let day = 1, weekday = firstDay; day <= lastDate; day++, weekday++) {
+    week = week.concat(String(day).padStart(3));
+    if (day === lastDate || weekday === 6) {
+      weeks.push(week.substring(1));
+      weekday = -1;
+      week = "";
+    }
+  }
+  return weeks.join("\n");
+};
+
+const formatCalender = (baseYear, baseMonth, baseCalender) => {
+  const calender = [
+    `      ${baseMonth}月 ${baseYear}`,
+    "日 月 火 水 木 金 土",
+    `${baseCalender}`,
+  ].join("\n");
+  return calender;
+};
+
 const today = new Date();
 const { m, y } = minimist(process.argv.slice(2));
 const baseYear = y || today.getFullYear();
 const baseMonth = m || today.getMonth() + 1;
-const month = baseMonth - 1;
-const firstDay = new Date(baseYear, month, 1).getDay();
-const lastDate = new Date(baseYear, month + 1, 0).getDate();
-const weeks = new Array();
-
-let firstWeek = "";
-if (firstDay !== 0) {
-  const padding = "   ".repeat(firstDay);
-  firstWeek = firstWeek.concat(padding);
-}
-let week = firstWeek;
-for (let day = 1, weekday = firstDay; day <= lastDate; day++, weekday++) {
-  week = week.concat(String(day).padStart(3));
-  if (day === lastDate || weekday === 6) {
-    weeks.push(week.substring(1));
-    weekday = -1;
-    week = "";
-  }
-}
-const calender = weeks.join("\n");
-const formatCalender = [
-  `       ${baseMonth}月 ${baseYear}`,
-  "日 月 火 水 木 金 土",
-  `${calender}`,
-].join("\n");
-
-console.log(formatCalender);
+const baseCalender = buildCalender(baseYear, baseMonth);
+const calender = formatCalender(baseYear, baseMonth, baseCalender);
+console.log(calender);
