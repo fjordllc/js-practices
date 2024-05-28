e6b0c8d49aa04ccc9d0c085a81f58fa26e282c80
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-05-28 10:24:03 +0900

    月取得時の分岐条件を変更、buildCalendarの責務変更に伴い戻り値とロジックの変更、sliceByNumber関数を作成

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -3,20 +3,20 @@
 import minimist from "minimist";
 
 const buildCalender = (baseYear, baseMonth) => {
-  const month = baseMonth;
-  const firstDay = new Date(baseYear, month, 1).getDay();
-  const lastDate = new Date(baseYear, month + 1, 0).getDate();
-  const weeks = [];
-  let week = "".concat("   ".repeat(firstDay));
-  for (let day = 1, weekday = firstDay; day <= lastDate; day++, weekday++) {
-    week = week.concat(String(day).padStart(3));
-    if (day === lastDate || weekday === 6) {
-      weeks.push(week.substring(1));
-      weekday = -1;
-      week = "";
-    }
-  }
-  return weeks.join("\n");
+  const firstDate = new Date(baseYear, baseMonth, 1);
+  const lastDate = new Date(baseYear, baseMonth + 1, 0);
+  const rawCalenderData = [...Array(lastDate.getDate())].map((_, i) => i + 1);
+  rawCalenderData.unshift(...Array(firstDate.getDay()));
+  const weeklyCalender = sliceByNumber(rawCalenderData, 7);
+  console.log(weeklyCalender);
+  return weeklyCalender;
+};
+
+const sliceByNumber = (array, number) => {
+  const length = Math.ceil(array.length / number);
+  return new Array(length)
+    .fill()
+    .map((_, i) => array.slice(i * number, (i + 1) * number));
 };
 
 const formatCalender = (baseYear, baseMonth, baseCalender) => {
@@ -31,14 +31,55 @@ const formatCalender = (baseYear, baseMonth, baseCalender) => {
 };
 
 const today = new Date();
-const argv = minimist(process.argv.slice(2), {
-  alias: {
-    y: "inputYear",
-    m: "inputMonth",
-  },
-});
-const baseYear = argv.inputYear ?? today.getFullYear();
-const baseMonth = argv.inputMonth - 1 ?? today.getMonth(); //Dateクラスの月の計算に沿うように、argv.inputMonthからは1マイナス
+const { y, m } = minimist(process.argv.slice(2));
+const inputYear = y;
+const inputMonth = m;
+const baseYear = inputYear ?? today.getFullYear();
+const baseMonth =
+  typeof inputMonth === "number" && 1 <= inputMonth <= 12
+    ? inputMonth - 1
+    : today.getMonth();
 const baseCalender = buildCalender(baseYear, baseMonth);
 const calender = formatCalender(baseYear, baseMonth, baseCalender);
 console.log(calender);
+
+// const buildCalender = (baseYear, baseMonth) => {
+//   const month = baseMonth;
+//   const firstDay = new Date(baseYear, month, 1).getDay();
+//   const lastDate = new Date(baseYear, month + 1, 0).getDate();
+//   const weeks = [];
+//   let week = "".concat("   ".repeat(firstDay));
+//   for (let day = 1, weekday = firstDay; day <= lastDate; day++, weekday++) {
+//     week = week.concat(String(day).padStart(3));
+//     if (day === lastDate || weekday === 6) {
+//       weeks.push(week.substring(1));
+//       weekday = -1;
+//       week = "";
+//     }
+//   }
+//   return weeks.join("\n");
+// };
+
+// const formatCalender = (baseYear, baseMonth, baseCalender) => {
+//   const displayMonth = baseMonth + 1;
+//   const formattedCalender =
+//     [
+//       `      ${displayMonth}月 ${baseYear}`,
+//       "日 月 火 水 木 金 土",
+//       baseCalender,
+//     ].join("\n") + "\n";
+//   return formattedCalender;
+// };
+
+// const today = new Date();
+// const argv = minimist(process.argv.slice(2), {
+//   alias: {
+//     y: "inputYear",
+//     m: "inputMonth",
+//   },
+// });
+// const baseYear = argv.inputYear ?? today.getFullYear();
+// const baseMonth = argv.inputMonth - 1 ?? today.getMonth(); //Dateクラスの月の計算に沿うように、argv.inputMonthからは1マイナス
+// const baseCalender = buildCalender(baseYear, baseMonth);
+// const calender = formatCalender(baseYear, baseMonth, baseCalender);
+// console.log(calender);
