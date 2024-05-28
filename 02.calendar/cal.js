ace6b68ac244f63913beecc9b3f7b203443b0c7b
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-05-28 10:25:00 +0900

    つづり間違いを修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -2,14 +2,14 @@
 
 import minimist from "minimist";
 
-const buildCalender = (baseYear, baseMonth) => {
+const buildCalendar = (baseYear, baseMonth) => {
   const firstDate = new Date(baseYear, baseMonth, 1);
   const lastDate = new Date(baseYear, baseMonth + 1, 0);
-  const rawCalenderData = [...Array(lastDate.getDate())].map((_, i) => i + 1);
-  rawCalenderData.unshift(...Array(firstDate.getDay()));
-  const weeklyCalender = sliceByNumber(rawCalenderData, 7);
-  console.log(weeklyCalender);
-  return weeklyCalender;
+  const rawCalendarData = [...Array(lastDate.getDate())].map((_, i) => i + 1);
+  rawCalendarData.unshift(...Array(firstDate.getDay()));
+  const weeklyCalendar = sliceByNumber(rawCalendarData, 7);
+  console.log(weeklyCalendar);
+  return weeklyCalendar;
 };
 
 const sliceByNumber = (array, number) => {
@@ -19,15 +19,15 @@ const sliceByNumber = (array, number) => {
     .map((_, i) => array.slice(i * number, (i + 1) * number));
 };
 
-const formatCalender = (baseYear, baseMonth, baseCalender) => {
+const formatCalendar = (baseYear, baseMonth, baseCalendar) => {
   const displayMonth = baseMonth + 1;
-  const formattedCalender =
+  const formattedCalendar =
     [
       `      ${displayMonth}月 ${baseYear}`,
       "日 月 火 水 木 金 土",
-      baseCalender,
+      baseCalendar,
     ].join("\n") + "\n";
-  return formattedCalender;
+  return formattedCalendar;
 };
 
 const today = new Date();
@@ -39,11 +39,11 @@ const baseMonth =
   typeof inputMonth === "number" && 1 <= inputMonth <= 12
     ? inputMonth - 1
     : today.getMonth();
-const baseCalender = buildCalender(baseYear, baseMonth);
-const calender = formatCalender(baseYear, baseMonth, baseCalender);
-console.log(calender);
+const baseCalendar = buildCalendar(baseYear, baseMonth);
+const calendar = formatCalendar(baseYear, baseMonth, baseCalendar);
+console.log(calendar);
 
-// const buildCalender = (baseYear, baseMonth) => {
+// const buildCalendar = (baseYear, baseMonth) => {
 //   const month = baseMonth;
 //   const firstDay = new Date(baseYear, month, 1).getDay();
 //   const lastDate = new Date(baseYear, month + 1, 0).getDate();
@@ -60,15 +60,15 @@ console.log(calender);
 //   return weeks.join("\n");
 // };
 
-// const formatCalender = (baseYear, baseMonth, baseCalender) => {
+// const formatCalendar = (baseYear, baseMonth, baseCalendar) => {
 //   const displayMonth = baseMonth + 1;
-//   const formattedCalender =
+//   const formattedCalendar =
 //     [
 //       `      ${displayMonth}月 ${baseYear}`,
 //       "日 月 火 水 木 金 土",
-//       baseCalender,
+//       baseCalendar,
 //     ].join("\n") + "\n";
-//   return formattedCalender;
+//   return formattedCalendar;
 // };
 
 // const today = new Date();
@@ -80,6 +80,6 @@ console.log(calender);
 // });
 // const baseYear = argv.inputYear ?? today.getFullYear();
 // const baseMonth = argv.inputMonth - 1 ?? today.getMonth(); //Dateクラスの月の計算に沿うように、argv.inputMonthからは1マイナス
-// const baseCalender = buildCalender(baseYear, baseMonth);
-// const calender = formatCalender(baseYear, baseMonth, baseCalender);
-// console.log(calender);
+// const baseCalendar = buildCalendar(baseYear, baseMonth);
+// const calendar = formatCalendar(baseYear, baseMonth, baseCalendar);
+// console.log(calendar);
