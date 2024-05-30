bc8caf80d1bfac8f659e0787b4712f88b05b2652
yuichi.imori <yuichi.imori@classi.jp>
2024-05-30 16:36:16 +0900

    コールバック処理を関数化

--- 03.asynchronous/asynchronous_noerror.js
+++ 03.asynchronous/asynchronous_noerror.js
@@ -1,41 +1,48 @@
 import sqlite3 from "sqlite3";
 
-const db = new sqlite3.Database(":memory:");
+function callback() {
+  const db = new sqlite3.Database(":memory:");
 
-db.serialize(() => {
-  db.run(
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    (err) => {
-      if (err) {
-        console.error("Table create error", err);
-        return;
-      }
-      console.log("Table created");
-
-      db.run("INSERT INTO books (title) VALUES ('あいうえお')", function (err) {
+  db.serialize(() => {
+    db.run(
+      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+      (err) => {
         if (err) {
-          console.error("Insert error", err);
+          console.error("Table create error", err);
           return;
         }
-        console.log("Insert book title with ID", this.lastID);
+        console.log("Table created");
 
-        db.all("SELECT * FROM books", (err, row) => {
-          if (err) {
-            console.error("Select error", err);
-            return;
-          }
-          console.log("Rows :", row);
-
-          db.run("DROP TABLE books", (err) => {
+        db.run(
+          "INSERT INTO books (title) VALUES ('あいうえお')",
+          function (err) {
             if (err) {
-              console.log("Drop error", err);
+              console.error("Insert error", err);
               return;
             }
-            console.log("Table dropped");
-            db.close();
-          });
-        });
-      });
-    },
-  );
-});
+            console.log("Insert book title with ID", this.lastID);
+
+            db.all("SELECT * FROM books", (err, row) => {
+              if (err) {
+                console.error("Select error", err);
+                return;
+              }
+              console.log("Rows :", row);
+
+              db.run("DROP TABLE books", (err) => {
+                if (err) {
+                  console.log("Drop error", err);
+                  return;
+                }
+                console.log("Table dropped");
+                db.close();
+              });
+            });
+          },
+        );
+      },
+    );
+  });
+}
+
+callback();
