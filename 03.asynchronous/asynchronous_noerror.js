86c80b0269c201c0fa45425a571c43b89df66a94
yuichi.imori <yuichi.imori@classi.jp>
2024-05-30 18:05:37 +0900

    Promise処理を追加

--- 03.asynchronous/asynchronous_noerror.js
+++ 03.asynchronous/asynchronous_noerror.js
@@ -45,4 +45,57 @@ function callback() {
   });
 }
 
+function promise() {
+  const db = new sqlite3.Database(":memory:");
+
+  function runPromise(db, query) {
+    return new Promise((resolve, reject) => {
+      db.run(query, function (err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(this);
+        }
+      });
+    });
+  }
+
+  function allPromise(db, query) {
+    return new Promise((resolve, reject) => {
+      db.all(query, (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  }
+
+  runPromise(
+    db,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  )
+    .then(() => {
+      console.log("Table created");
+      return runPromise(db, "INSERT INTO books (title) VALUES ('あいうえお')");
+    })
+    .then((result) => {
+      console.log("Insert book title with ID", result.lastID);
+      return allPromise(db, "SELECT * FROM books");
+    })
+    .then((rows) => {
+      console.log("Rows: ", rows);
+      return runPromise(db, "DROP TABLE books");
+    })
+    .then(() => {
+      console.log("Table dropped");
+      db.close();
+    })
+    .catch((err) => {
+      console.error("Error: ", error);
+    });
+}
+
 callback();
+promise();
