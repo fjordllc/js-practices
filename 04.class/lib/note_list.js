1a280ce6be5e96a78cad7bed51cc3b21fbaf5dde
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-06-27 16:13:06 +0900

    クラス名を変更(DisplayからNoteListへ)、関数名を変更

---
+++ 04.class/lib/note_list.js
@@ -0,0 +1,32 @@
+import Enquirer from "enquirer";
+
+export class NoteList {
+  constructor(notes) {
+    this._notes = notes;
+  }
+
+  get notes() {
+    return this._notes;
+  }
+
+  seeAllTitles() {
+    this._notes.forEach((note) => console.log(note.title));
+  }
+
+  async selectNote(messageText) {
+    const question = {
+      type: "select",
+      name: "toSeeNote",
+      message: messageText,
+      choices: this.notes.map((note) => ({
+        name: note.title,
+        value: note,
+      })),
+      result() {
+        return this.focused.value;
+      },
+    };
+    const answer = await Enquirer.prompt(question);
+    return answer.toSeeNote;
+  }
+}
