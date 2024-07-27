b8315e7aead7086619bbf4f1a3045db63097e5c2
kurosu <kurosu.66@gmail.com>
2024-07-27 17:31:24 +0900

    whileからforに変更

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,15 +1,13 @@
 #!/usr/bin/env node
 
-let n = 1;
-while (n <= 20) {
-  if (n % 15 == 0) {
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
     console.log("FizzBuzz");
-  } else if (n % 3 == 0) {
+  } else if (i % 3 == 0) {
     console.log("Fizz");
-  } else if (n % 5 == 0) {
+  } else if (i % 5 == 0) {
     console.log("Buzz");
   } else {
-    console.log(n);
+    console.log(i);
   }
-  n++;
 }
