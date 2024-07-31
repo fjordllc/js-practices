5830ca42ec85c92e87899ab96e0ec74b12fa4956
jyouma <juri19771211@gmail.com>
2024-07-31 21:40:47 +0900

    Update fizzbuzz.js

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,11 +1,11 @@
 #!/user/bin/env node
 
-for(let i =1; i <= 20; i++) {
-  if( i % 3 === 0 && i % 5=== 0) {
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
     console.log("FizzBuzz");
-  } elsif (i % 3 === 0) {
+  } else if (i % 3 === 0) {
     console.log("Fizz");
-  } elsif (i % 5 === 0) {
+  } else if (i % 5 === 0) {
     console.log("Buzz");
   } else {
     console.log(i);
