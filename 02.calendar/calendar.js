b22df40c91067f22204a43d93cdf219301188c0a
jyouma <juri19771211@gmail.com>
2024-08-15 17:19:09 +0900

    calendar.jsのmonthとyearをLuxonのDateTimeオブジェクトとする

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -5,27 +5,25 @@ import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
 const now = LuxonDateTime.now();
-const month = args.m || now.month;
-const year = args.y || now.year;
+const date = LuxonDateTime.local(args.y || now.year, args.m || now.month, 1);
 
-
-if (month < 1 || month > 12) {
-  console.error('Month must be between 1 and 12.');
+if (date.month < 1 || date.month > 12) {
+  console.error("Month must be between 1 and 12.");
   process.exit(1);
 }
 
-if (year < 1970 || year > 2100) {
-  console.error('Year must be between 1970 and 2100.');
+if (date.year < 1970 || date.year > 2100) {
+  console.error("Year must be between 1970 and 2100.");
   process.exit(1);
 }
 
-const firstDateOfMonth = LuxonDateTime.local(year, month, 1);
+const firstDateOfMonth = date.startOf("month");
 const lastDateOfMonth = firstDateOfMonth.endOf("month");
 const daysInMonth = lastDateOfMonth.day;
 const firstWeekday = firstDateOfMonth.weekday === 7 ? 0 : firstDateOfMonth.weekday;
 
-console.log ("日 月 火 水 木 金 土");
 console.log(`\n     ${firstDateOfMonth.month}月 ${firstDateOfMonth.year}`);
+console.log ("日 月 火 水 木 金 土");
 
 let dayString = '';
 for(let i = 0; i < firstWeekday; i++) {
