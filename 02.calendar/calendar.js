e7e48c2e29fd440d8d77fd77e36e2a8813f99c2f
jyouma <juri19771211@gmail.com>
2024-08-18 01:02:39 +0900

    calコマンドと同様に月が日本語表示になるように修正

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -5,14 +5,17 @@ import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
 const now = LuxonDateTime.now();
-const date = LuxonDateTime.local(args.y || now.year, args.m || now.month, 1);
+const year = args.y || now.year;
+const month = args.m || now.month;
 
-if (date.month < 1 || date.month > 12) {
+const date = LuxonDateTime.local(year, month, 1);
+
+if (month < 1 || month > 12) {
   console.error("Month must be between 1 and 12.");
   process.exit(1);
 }
 
-if (date.year < 1970 || date.year > 2100) {
+if (year < 1970 || year > 2100) {
   console.error("Year must be between 1970 and 2100.");
   process.exit(1);
 }
@@ -22,8 +25,8 @@ const lastDateOfMonth = firstDateOfMonth.endOf("month");
 const daysInMonth = lastDateOfMonth.day;
 const firstWeekday = firstDateOfMonth.weekday % 7;
 
-console.log(`\n     ${firstDateOfMonth.month}月 ${firstDateOfMonth.year}`);
-console.log ("日 月 火 水 木 金 土");
+console.log(`\n     ${firstDateOfMonth.setLocale('ja').toFormat('M月 yyyy')}`);
+console.log("日 月 火 水 木 金 土");
 
 let dayString = " ".repeat(firstWeekday * 3);
 
