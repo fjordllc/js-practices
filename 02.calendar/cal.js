400fcf5c4c34eb5129879f496ebcec6c5e34d336
jyouma <juri19771211@gmail.com>
2024-09-02 12:19:45 +0900

    calendar.jsの修正に伴うcal.jsの修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -18,26 +18,36 @@ if (year < 1970 || year > 2100) {
   process.exit(1);
 }
 
-const startDate = luxon.DateTime.local(year, month, 1);
-const endDate = startDate.endOf("month");
-
-const monthNames = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
-const header = monthNames[month - 1] + " " + startDate.year;
+const monthNames = [
+  "1月",
+  "2月",
+  "3月",
+  "4月",
+  "5月",
+  "6月",
+  "7月",
+  "8月",
+  "9月",
+  "10月",
+  "11月",
+  "12月",
+];
+const header = `${monthNames[month - 1]} ${year}`;
 const padding = Math.floor((20 - header.length) / 2);
 console.log(" ".repeat(padding) + header);
 console.log("日 月 火 水 木 金 土");
 
-let dayString = " ".repeat((startDate.weekday % 7) * 3);
+const startDay = luxon.DateTime.local(year, month, 1).weekday % 7;
+let dayString = " ".repeat(startDay * 3);
 
-for (
-  let currentDate = startDate;
-  currentDate <= endDate;
-  currentDate = currentDate.plus({ days: 1 })
-) {
-  dayString += `${currentDate.day.toString().padStart(2, " ")}`;
+const daysInMonth = luxon.DateTime.local(year, month).daysInMonth;
 
-  if (currentDate.weekday === 6 || currentDate.day === endDate.day) {
-    console.log(dayString.trimEnd());
+for (let day = 1; day <= daysInMonth; day++) {
+  const currentDate = luxon.DateTime.local(year, month, day);
+  dayString += day.toString().padStart(2, " ");
+
+  if (currentDate.weekday === 6 || day === daysInMonth) {
+    console.log(dayString);
     dayString = "";
   } else {
     dayString += " ";
