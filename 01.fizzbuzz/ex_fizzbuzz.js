02b4a5e36299d5630c03d52dca037e3c1630fec3
itagaki <itagaki_shun@vitallead.co.jp>
2025-04-14 11:33:25 +0900

    課題プログラムの作成

---
+++ 01.fizzbuzz/ex_fizzbuzz.js
@@ -0,0 +1,16 @@
+let cnt = 0;
+while(cnt < 20){
+    cnt++;
+    if(cnt % 3 == 0 && cnt % 5 == 0){
+        console.log("FizzBuzz");
+    }
+    else if(cnt % 3 == 0){
+        console.log("Fizz");
+    }
+    else if(cnt % 5 == 0){
+        console.log("Buzz");
+    }
+    else{
+    console.log(cnt);
+    };
+}
