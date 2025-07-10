b4dcf96ca61c2ccc23f5b5fe5701c291fd2309c0
minami <katominami0812@gmail.com>
2025-07-11 00:05:27 +0900

    fizzbuzzのJavaScript

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,13 @@
+for (let number = 1; number <= 20; number++) {
+  let output = "";
+  if (number % 3 === 0 && number % 5 === 0) {
+    output = "FizzBuzz";
+  } else if (number % 3 === 0) {
+    output = "Fizz";
+  } else if (number % 5 === 0) {
+    output = "Buzz";
+  } else {
+    output = number;
+  }
+    console.log(JSON.stringify(output));
+}
