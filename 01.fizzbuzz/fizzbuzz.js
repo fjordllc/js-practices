41cb4919e44465376f5627c554e7ea183fb74b2d
mikan-v7y <mikan.v7y@gmail.com>
2025-09-04 03:43:58 +0900

    refactor: 可読性向上のため、PrettierのPlaygroundでコードをフォーマットする

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,12 +1,11 @@
-for(let i = 1; i <= 20; i++){
+for (let i = 1; i <= 20; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log('FizzBuzz')
-  } else if (i % 3 === 0){
-    console.log('Fizz')
-  } else if (i % 5 === 0){
-    console.log('Buzz')
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
   } else {
-    console.log(i)
+    console.log(i);
   }
 }
-
