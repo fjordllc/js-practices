df678f39e8e4ef6208e6fb948527ca846d019e70
五味佑麻 <YumaGomi@MacBook-Air.local>
2025-09-29 11:43:56 +0900

    my-fizzbuzz

--- 01.fizzbuzz/FizzBuzz.js
+++ 01.fizzbuzz/FizzBuzz.js
@@ -1,14 +1,11 @@
 for (let cnt = 1; cnt <= 20; cnt++) {
-    if( cnt % 3 === 0) {
-        console.log("Fizz");
-    }
-    if (cnt % 5 === 0) {
-        console.log("Buzz");
-    }
-    if (cnt % 3 == 0 && cnt % 5 == 0) {
+    if (cnt % 3 === 0 && cnt % 5 === 0) {
         console.log("FizzBuzz");
-    }
-    else {
+    } else if (cnt % 3 === 0) {
+        console.log("Fizz");
+    } else if (cnt % 5 === 0) {
+        console.log("Buzz");
+    } else {
         console.log(cnt);
     }
 }
